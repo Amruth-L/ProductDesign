7351706a1438333490e4190fd614ab441eb161a1
Amruth-L <amruthln21@gmail.com>
2026-05-04 23:14:54 +0530

    changed 1

--- src/components/ProductGrid.jsx
+++ src/components/ProductGrid.jsx
@@ -1,7 +1,7 @@
 import ProductCard from './ProductCard';
 const ProductGrid = ({ items }) => {
     return (
-        <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6'>
+        <div className='grid grid-cols-3 sm:grid-cols-2 lg:grid-cols-5 gap-6'>
             {items.map((item) => (
                 <ProductCard key={item.id} product={item} />))}
         </div>
