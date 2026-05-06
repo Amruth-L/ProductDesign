f04040c6e61c4ebe7b119415fd671b4b148f948a
Amruth-L <amruthln21@gmail.com>
2026-05-06 20:49:14 +0530

    description modified

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -4,9 +4,14 @@ const ProductCard = ({ product }) => {
             <div className="h-40 flex justify-center items-center mb-4">
                 <img src={product.image} alt={product.title} className="max-h-full" />
             </div>
-            <h3 className="text-gray-800 font-semibold truncate">{product.title}</h3>
+            <h3 className="text-gray-800 font-semibold truncate">${product.title}</h3>
+            <h3 className="text-blue-300 font-semibold truncate">{product.category}</h3>
             <p className="text-blue-600 font-bold mt-2">$ {product.price}</p>
-            <p className="text-gray-500 text-sm mt-2 line-clamp-2">${product.description}</p>
+            <p className="mt-3 text-xs uppercase tracking-widest text-blue-600 font-semibold">Details</p>
+            <div className="relative mt-4 overflow-hidden">
+                <p className="text-gray-500 text-sm line-clamp-2 group:hover:text-gray-800 transition-colors duration-300">${product.description}
+                </p>
+            </div>
         </div>
     );
 };
